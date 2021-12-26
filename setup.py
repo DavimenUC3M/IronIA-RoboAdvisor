30f59eb7de9fb71c6cade6a791b9076dcc634524
David Méndez Encinas <ramitadeficus@gmail.com>
2021-12-26 16:10:56 +0100

    Added setup and minor changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='IronIA RoboAdvisor for Portfolio Optimization',
+    author='BDCL',
+    license='MIT',
+)
