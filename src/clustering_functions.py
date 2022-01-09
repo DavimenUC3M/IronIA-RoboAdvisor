83398fc3b4b324e51f3171e839c45df4709639c3
David Méndez Encinas <ramitadeficus@gmail.com>
2022-01-09 19:01:36 +0100

    Added Columns to Cluster datframe

--- src/clustering_functions.py
+++ src/clustering_functions.py
@@ -11,7 +11,7 @@ import os
 import re
 
 
-def calculate_cluster_variables(start,end,colnames,terminations,dfs,verbose=False,column_name="Average"): 
+def calculate_cluster_variables(start,end,colnames,terminations,dfs,gdps,inflation,verbose=False,column_name="Average"): 
 
     """
     start: first date to take into account
@@ -24,10 +24,10 @@ def calculate_cluster_variables(start,end,colnames,terminations,dfs,verbose=Fals
 
     """
 
-    offsets = []
-    for i in terminations:
-        s = ([float(s) for s in re.findall(r'-?\d+\.?\d*', i)]) #Regular expression to get just the numbers
-        offsets.append(int(s[0]))
+    # offsets = []
+    # for i in terminations:
+    #     s = ([float(s) for s in re.findall(r'-?\d+\.?\d*', i)]) #Regular expression to get just the numbers
+    #     offsets.append(int(s[0]))
 
 
     start_date = date.fromisoformat(start)
@@ -38,19 +38,41 @@ def calculate_cluster_variables(start,end,colnames,terminations,dfs,verbose=Fals
     
     for rows in range(num_months+1):
         start_date_offset = start_date + relativedelta(months=rows)
+        year = str(start_date_offset.year)
+        month = str(start_date_offset.month)
         if verbose:
             print(start_date_offset)
         row = []
         for df in dfs:
-            #offsets = [12,6,3,2,1] #This offsets must follow the terminations
+            offsets = [12,9,6,3,2,1] #This offsets must follow the terminations
+            weights = np.array([0.07,0.09,0.12,0.15,0.22,0.35]) #Weights of ponderated mean
+            variances = []
+            changes = []
+            difference = []
             for offset in offsets:
+
                 offset_date = start_date_offset - relativedelta(months=offset)
-                try:
-                    row.append(df[offset_date.isoformat():start_date_offset.isoformat()][column_name].var())
-                except:
-                    break #Just skip the datasets that doesnt follow the general rules
-                
+                variances.append(df[offset_date.isoformat():start_date_offset.isoformat()][column_name].var())
+                #changes.append(df[offset_date.isoformat():start_date_offset.isoformat()][column_name].pct_change().mean())
+                difference.append(df[offset_date.isoformat():start_date_offset.isoformat()][column_name].diff().mean())
+
+            if len(variances) == len(weights):
+                variances = np.array(variances)
+                row.append(sum(variances*weights))
+                #row.append(sum(changes*weights))
+                row.append(sum(difference*weights))
+
+        for gdp in gdps:
+            row.append(gdp[year]["GDP"].values[0])
+
+        row.append(inflation[f"{year}-{month}"]["Inflation"].values[0])
+
+        row.append(str(start_date_offset.isoformat()))
+        
         clustering_df.loc[len(clustering_df)] = row
+
+    clustering_df["Date"] = pd.to_datetime(clustering_df["Date"])
+    clustering_df.set_index("Date",inplace=True)
     
     return clustering_df
 
@@ -73,6 +95,8 @@ def plot_elbow_graph(df,use_PCA=False,n_pca_components=2,verbose=False):
 
 def make_clustering(df,n_clusters,use_PCA=False,n_pca_components=2,verbose=False):
 
+    pca = None
+
     if use_PCA:
         pca = PCA(n_components=n_pca_components)
         pca.fit(df)
@@ -85,11 +109,15 @@ def make_clustering(df,n_clusters,use_PCA=False,n_pca_components=2,verbose=False
 
     if verbose:
         plt.scatter(centroids[:,0] , centroids[:,1] , s = 300, marker="*")
-        plt.legend(["Centroids"])
-        plt.scatter(df[:,0],df[:,1],c=labels)
+        
         plt.xlabel('Component 1')
         plt.ylabel('Component 2')
         plt.title('Clustering 2D Graph')
-        
+        scatter = plt.scatter(df[:,0],df[:,1], c = labels)
+        labels_ = np.unique(labels)
+        handles = [plt.Line2D([],[],marker="o", ls="", color=scatter.cmap(scatter.norm(yi))) for yi in labels_]
+        legends_ = plt.legend(["Centroids"],loc=(0.921,0.76))
+        plt.legend(handles, labels_, title="Cluster Label",loc="upper right")
+        plt.gca().add_artist(legends_)
 
-    return([kmeans,labels]) #Returns the fitted kmeans and the labels of the train data
+    return([kmeans,pca,labels]) #Returns the fitted kmeans, the pca projection and the labels of the train data
